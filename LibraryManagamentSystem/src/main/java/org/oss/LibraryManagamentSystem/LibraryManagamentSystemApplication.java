c7e4ce6cd85eaca4084432ed5c1642e7d31b0679
Petar1107 <pv48370@oss.unist.hr>
2022-12-04 20:41:25 +0100

    Spring Boot project init

---
+++ LibraryManagamentSystem/src/main/java/org/oss/LibraryManagamentSystem/LibraryManagamentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.oss.LibraryManagamentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagamentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagamentSystemApplication.class, args);
+	}
+
+}
---
+++ LibraryManagamentSystem/src/test/java/org/oss/LibraryManagamentSystem/LibraryManagamentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oss.LibraryManagamentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagamentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
