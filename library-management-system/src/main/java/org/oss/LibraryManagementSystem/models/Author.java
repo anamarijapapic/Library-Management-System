db36782093a17e04bcf0c098aaa3effc75e87db5
Petar1107 <pv48370@oss.unist.hr>
2023-01-07 19:52:18 +0100

    Create model: Author

---
+++ library-management-system/src/main/java/org/oss/LibraryManagementSystem/models/Author.java
@@ -0,0 +1,31 @@
+package org.oss.LibraryManagementSystem.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "first_name")
+    @NotNull(message = "first_name shouldn't be null")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotNull(message = "last_name shouldn't be null")
+    private String lastName;
+
+}
